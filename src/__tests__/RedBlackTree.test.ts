a8d182e864eda579d9f6e08a689c57457e47dc74
Lydia Yuan <lydiayuan99@gmail.com>
2023-07-28 16:03:52 -0600

    draft: add jest test template

---
+++ src/__tests__/RedBlackTree.test.ts
@@ -0,0 +1,29 @@
+import { RedBlackTree } from '../apis/RedBlackTree';
+
+describe('RedBlackTree', () => {
+  let rbTree: RedBlackTree;
+
+  beforeEach(() => {
+    rbTree = new RedBlackTree();
+    // Optionally, you can insert some test data here
+  });
+
+  it('should insert elements correctly', () => {
+    // Write test cases to verify correct insertion
+  });
+
+  it('should delete elements correctly', () => {
+    // Write test cases to verify correct deletion
+  });
+
+  it('should find elements correctly', () => {
+    // Write test cases to verify correct search
+  });
+
+  it('should perform rotations correctly', () => {
+    // Write test cases to verify correct rotations
+  });
+
+  // Other test cases for edge cases, tree properties, etc.
+
+});
