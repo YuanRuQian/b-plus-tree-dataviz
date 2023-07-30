c93336d96c879a279fdf46cd7c9658f10cc5f65e
Lydia Yuan <lydiayuan99@gmail.com>
2023-07-29 19:50:53 -0600

    chore: add transition

--- src/components/DataVisualization.tsx
+++ src/components/DataVisualization.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
-import { TreeNodeJSON } from "../utils/Node";
+import { TreeNodeJSON } from "../utils/Node";;
 
 type DataVisualizationProps = {
   redBlackTreeData: TreeNodeJSON | null;
 };
 
 const DataVisualization = ({ redBlackTreeData }: DataVisualizationProps) => {
-
-  console.log(`redBlackTreeData: ${JSON.stringify(redBlackTreeData)}`)
-
   const treeContainerRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
-    
+
+    console.log(`redBlackTreeData: ${JSON.stringify(redBlackTreeData)}`)
+
     if (!redBlackTreeData || JSON.stringify(redBlackTreeData) === '{}') {
-      // first clear all the elements then return early
+      // If the data is null or empty, clear all the elements then return early
       d3.select(treeContainerRef.current).selectAll("*").remove();
       return;
     }
@@ -29,63 +28,84 @@ const DataVisualization = ({ redBlackTreeData }: DataVisualizationProps) => {
     // Compute the layout
     const treeRoot = treeLayout(root);
 
-    // Remove the old tree by cleaning up the SVG container
-    d3.select(treeContainerRef.current).selectAll("*").remove();
-
-    // Create an SVG group to contain the tree
+    // Select the SVG element
     const svg = d3.select(treeContainerRef.current);
-    const treeGroup = svg.append("g").attr("transform", "translate(100, 50)");
 
-    
     // Add links (edges) between nodes
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const links = treeGroup
+    const links = svg
       .selectAll<SVGPathElement, d3.HierarchyPointLink<any>>(".link")
-      .data(treeRoot.links())
-      .enter()
+      .data(treeRoot.links());
+
+    // Enter new links with smooth transitions
+    links.enter()
       .append("path")
       .attr("class", "link")
-      .attr(
-        "d",
-        d3
-          .linkHorizontal<
-            d3.HierarchyPointLink<any>,
-            d3.HierarchyPointNode<any>
-          >()
-          .x((d) => d.x!)
-          .y((d) => d.y!),
-      );
+      .attr("d", d3.linkHorizontal<d3.HierarchyPointLink<any>, d3.HierarchyPointNode<any>>()
+        .x((d) => d.x!)
+        .y((d) => d.y!))
+      .merge(links) // Merge existing and new links
+      .transition() // Apply a smooth transition
+      .duration(1000) // Transition duration in milliseconds
+      .attr("d", d3.linkHorizontal<d3.HierarchyPointLink<any>, d3.HierarchyPointNode<any>>()
+        .x((d) => d.x!)
+        .y((d) => d.y!));
+
+    // Remove any links that are no longer in the data
+    links.exit().remove();
 
     // Add nodes to the tree
-    const nodes = treeGroup
+    const nodes = svg
       .selectAll<SVGGElement, d3.HierarchyPointNode<any>>(".node")
-      .data(treeRoot.descendants())
-      .enter()
+      .data(treeRoot.descendants());
+
+    // Enter new nodes with smooth transitions
+    const enteredNodes = nodes.enter()
       .append("g")
       .attr("class", "node")
       .attr("transform", (d) => `translate(${d.x},${d.y})`);
 
-    // Add circles as node elements
-    nodes
-      .append("circle")
-      .attr("r", '1rem') // Set the radius of the circles
-      .attr("fill", (d) => d.data.color); // Customize the fill color based on the "color" property
+    // Append circles to the entered nodes and set their fill color based on the node data
+    enteredNodes.append("circle")
+      .attr("r", '1rem')
+      .attr("fill", (d) => d.data.color); // Corrected color assignment
 
-    // Add text labels for nodes
-    nodes
-      .append("text")
-      .style("text-anchor", "middle") // Center the text horizontally
-      .attr("fill", "white") // Set the text color to white
+    // Append text labels to the entered nodes
+    enteredNodes.append("text")
+      .style("text-anchor", "start") // Set the text-anchor to "start" to align the text to the left
+      .attr("fill", "black")
       .text((d) => d.data.name)
       .each(function (d) {
-        // Get the bounding box of the text element
-        const bbox = this.getBBox();
-        // Calculate the translation to center the text within the circle
-        const xTranslation = -bbox.width / 4;
-        const yTranslation = bbox.height / 4; // Adjust the vertical position as needed
+        const xTranslation = 30; // Change this value to adjust the horizontal position
+        const yTranslation = 10;
         d3.select(this).attr("transform", `translate(${xTranslation}, ${yTranslation})`);
       });
 
+    // Merge existing and new nodes, then apply a smooth transition
+    const mergedNodes = enteredNodes.merge(nodes);
+
+    // Update the fill color of existing nodes based on the node data
+    mergedNodes.select("circle")
+      .transition()
+      .duration(1000)
+      .attr("fill", (d) => d.data.color);
+
+    // Update the text content of existing nodes based on the node data
+    mergedNodes.select("text")
+      .transition()
+      .duration(1000)
+      .text((d) => d.data.name)
+      .each(function (d) {
+        const xTranslation = 30; // Change this value to adjust the horizontal position
+        const yTranslation = 10;
+        d3.select(this).attr("transform", `translate(${xTranslation}, ${yTranslation})`);
+      });
+
+    mergedNodes.transition()
+      .duration(1000)
+      .attr("transform", (d) => `translate(${d.x},${d.y})`);
+
+    // Remove any nodes that are no longer in the data
+    nodes.exit().remove();
   }, [redBlackTreeData]);
 
   return (
