425b5350ae57348caae9977cebf6e5217d7ec552
Lydia Yuan <lydiayuan99@gmail.com>
2023-07-29 19:03:40 -0600

    fix: input lose focus

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,81 +1,107 @@
 import React from "react";
-import { AppBar as MuiAppBar, Toolbar, Button, Container } from "@mui/material";
-import ButtonWithNumberInputAndTooltip from "./ButtonWithNumberInput";
+import { AppBar as MuiAppBar, Toolbar, Button, TextField, Box } from "@mui/material";
+import { isUndefined } from "../utils/utils";
 
 type AppBarProps = {
-  insertValue: number | undefined;
-  deleteValue: number | undefined;
-  findValue: number | undefined;
-  setInsertValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  setDeleteValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  setFindValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleInsert: () => void;
-  handleDelete: () => void;
-  handleFind: () => void;
+  handleInsert: (value : number) => void;
+  handleDelete: (value : number) => void;
+  handleFind: (value : number) => void;
   handleClearAll: () => void;
 };
 
+type CustomButtonProps = {
+  onClick: () => void;
+  buttonLabel: string;
+};
+
+const CustomButton = ({onClick, buttonLabel}: CustomButtonProps) => (
+  <div>
+    <Button variant="outlined" onClick={onClick}>
+      {buttonLabel}
+    </Button>
+  </div>
+);
+
+type CustomNumberInputProps = {
+  value: number | undefined;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  inputLabel: string;
+};
+
+const CustomNumberInput = ({ value, onChange, inputLabel }: CustomNumberInputProps) => (
+  <TextField
+    type="number"
+    label={inputLabel}
+    value={value || ""}
+    onChange={onChange}
+  />
+);
+
 const AppBar: React.FC<AppBarProps> = ({
-  insertValue,
-  deleteValue,
-  findValue,
-  setInsertValue,
-  setDeleteValue,
-  setFindValue,
   handleInsert,
   handleDelete,
   handleFind,
   handleClearAll,
 }) => {
+
+  const [input, setInput] = React.useState<number | undefined>(undefined);
+
+  const handleInputChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(parseFloat(event.target.value));
+  }
+
+  const handleInsertButtonClick = () => {
+    if(!isUndefined(input)) {
+      handleInsert(input);
+      setInput(undefined);
+      console.log(`Inserting ${input}`);
+    }
+  }
+
+  const handleDeleteButtonClick = () => {
+    if(!isUndefined(input)) {
+      handleDelete(input);
+      setInput(undefined);
+      console.log(`Deleting ${input}`);
+    }
+  }
+
+  const handleFindButtonClick = () => {
+    if(!isUndefined(input)) {
+      handleFind(input);
+      setInput(undefined);
+      console.log(`Finding ${input}`);
+    }
+  }
+
   const InsertButtonWithInput = () => (
-    <ButtonWithNumberInputAndTooltip
-      buttonLabel="Insert"
-      inputLabel="Insert Value"
-      value={insertValue}
-      onChange={setInsertValue}
-      onClick={handleInsert}
-    />
-  );
+    <CustomButton onClick={handleInsertButtonClick} buttonLabel="Insert" />
+  )
 
   const DeleteButtonWithInput = () => (
-    <ButtonWithNumberInputAndTooltip
-      buttonLabel="Delete"
-      inputLabel="Delete Value"
-      value={deleteValue}
-      onChange={setDeleteValue}
-      onClick={handleDelete}
-    />
+    <CustomButton onClick={handleDeleteButtonClick} buttonLabel="Delete" />
   );
 
   const FindButtonWithInput = () => (
-    <ButtonWithNumberInputAndTooltip
-      buttonLabel="Find"
-      inputLabel="Find Value"
-      value={findValue}
-      onChange={setFindValue}
-      onClick={handleFind}
-    />
-  );
+    <CustomButton onClick={handleFindButtonClick} buttonLabel="Find" />
+  )
 
   const ClearAllButton = () => (
-    <div>
-      <Button variant="outlined" onClick={handleClearAll}>
-        Clear All
-      </Button>
-    </div>
+    <CustomButton onClick={handleClearAll} buttonLabel="Clear All" />
   );
 
   return (
-    <MuiAppBar position="static" color="transparent">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <Box sx={{ flexGrow: 1 }}>
+    <MuiAppBar position="fixed" color="transparent">
+        <Toolbar disableGutters style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <CustomNumberInput value={input} onChange={handleInputChanges} inputLabel="Enter a number" />
           <InsertButtonWithInput />
           <DeleteButtonWithInput />
           <FindButtonWithInput />
           <ClearAllButton />
         </Toolbar>
-      </Container>
     </MuiAppBar>
+    </Box>
   );
 };
 
--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -3,90 +3,46 @@ import AppBar from "./AppBar";
 import DataVisualization from "./DataVisualization";
 import { TreeNodeJSON } from "../utils/Node";
 import { RedBlackTreeContext } from "../context/RedBlackTreeContext";
-import { isNull, isUndefined } from "../utils/utils";
+import { isNull } from "../utils/utils";
 
 const ControlPanel = () => {
   const context = useContext(RedBlackTreeContext);
 
-  const [insertValue, setInsertValue] = useState<number>();
-  const [deleteValue, setDeleteValue] = useState<number>();
-  const [findValue, setFindValue] = useState<number>();
   const [redBlackTreeData, setRedBlackTreeData] = useState<TreeNodeJSON>({} as TreeNodeJSON);
 
-  const setInsertValueHandler = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const newValue = parseFloat(event.target.value);
-    setInsertValue(newValue);
+  const handleInsert = (value: number) => {
+    if (!isNull(context)) {
+      context.redBlackTree.insert(value);
+      setRedBlackTreeData(context.redBlackTree.getInOrderTraversalPath() || {} as TreeNodeJSON);
+    }
   };
 
-  const setDeleteValueHandler = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const newValue = parseFloat(event.target.value);
-    setDeleteValue(newValue);
-  };
-
-  const setFindValueHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseFloat(event.target.value);
-    setFindValue(newValue);
-  };
-
-  const handleInsert = () => {
-    if(!isUndefined(insertValue) && !isNull(context)) {
-      context.redBlackTree.insert(insertValue);
-      console.log(`Inserted: ${insertValue}`);
-
-      // clear the input field
-      setInsertValue(undefined);
-
-      // update the red black tree data source
+  const handleDelete = (value: number) => {
+    if (!isNull(context)) {
+      context.redBlackTree.delete(value);
       setRedBlackTreeData(context.redBlackTree.getInOrderTraversalPath() || {} as TreeNodeJSON);
     }
   }
 
-  const handleDelete = () => {
-    if(!isUndefined(deleteValue) && !isNull(context)) {
-      context.redBlackTree.delete(deleteValue);
-      console.log(`Deleted: ${deleteValue}`);
+  const handleFind = (value: number) => {
+    if (!isNull(context)) {
+      context.redBlackTree.find(value);
 
-      // clear the input field
-      setDeleteValue(undefined);
-
-      // update the red black tree data source
-      setRedBlackTreeData(context.redBlackTree.getInOrderTraversalPath() || {} as TreeNodeJSON);
-    }
-  }
-
-  const handleFind = () => {
-    if(!isUndefined(findValue) && !isNull(context)) {
-      const res = context.redBlackTree.find(findValue);
-      console.log(res ? `Found: ${findValue}` : `Not found: ${findValue}`);
-
-      // clear the input field
-      setFindValue(undefined);
+      // TODO: do something with the result of find
     }
   }
 
   const handleClearAll = () => {
-    if(!isNull(context)) {
+    if (!isNull(context)) {
       context.redBlackTree.clear();
-      console.log("Cleared");
-
-      // update the red black tree data source
       setRedBlackTreeData(context.redBlackTree.getInOrderTraversalPath() || {} as TreeNodeJSON);
     }
   }
 
+
   return (
     <div>
       <AppBar
-        insertValue={insertValue}
-        deleteValue={deleteValue}
-        findValue={findValue}
-        setInsertValue={setInsertValueHandler}
-        setDeleteValue={setDeleteValueHandler}
-        setFindValue={setFindValueHandler}
         handleInsert={handleInsert}
         handleDelete={handleDelete}
         handleFind={handleFind}
