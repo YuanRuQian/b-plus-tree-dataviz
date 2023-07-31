bcbb697963b2a6c99f5725fd18ae2713bb6e0e55
Lydia Yuan <lydiayuan99@gmail.com>
2023-07-31 16:46:09 -0600

    chore: set first node's position

--- src/components/TreeChart.tsx
+++ src/components/TreeChart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Tree, {
   CustomNodeElementProps,
   RawNodeDatum,
@@ -11,6 +11,18 @@ type TreeChartProps = {
 };
 
 const TreeChart = ({ redBlackTreeData }: TreeChartProps) => {
+
+  const treeContainerRef = useRef<HTMLDivElement>(null);
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    
+    setSize({ width: window.innerWidth, height: window.innerHeight });
+    setTranslate({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+  }, []);
+
+
   if (JSON.stringify(redBlackTreeData) === JSON.stringify({})) return <></>;
 
   const renderCustomNodeElement = ({ nodeDatum }: CustomNodeElementProps) => (
@@ -29,13 +41,18 @@ const TreeChart = ({ redBlackTreeData }: TreeChartProps) => {
 
   // TODO: proper transformation of tree data
   return (
+    <div ref={treeContainerRef} style={{ width: size.width, height: size.height }}>
     <Tree
       orientation="vertical"
+      translate={translate}
       data={redBlackTreeData}
       renderCustomNodeElement={renderCustomNodeElement}
       pathClassFunc={getDynamicPathClass}
     />
+    </div>
   );
 };
 
+
 export default TreeChart;
+
